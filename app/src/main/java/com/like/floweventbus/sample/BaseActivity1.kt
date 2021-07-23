d27311029da24fc34715162b360c80230880b7ae
like5188 <260864866@qq.com>
2021-07-23 17:11:06 +0800

    完善

--- app/src/main/java/com/like/floweventbus/sample/BaseActivity1.kt
+++ app/src/main/java/com/like/floweventbus/sample/BaseActivity1.kt
@@ -6,9 +6,9 @@ import com.like.floweventbus_annotations.BusObserver
 
 open class BaseActivity1 : BaseActivity() {
 
-//    @BusObserver(["like5"])
-//    fun test1(i: Int) {
-//        Log.e(TAG, "BaseActivity1 test tag=like5，数据：$i")
-//    }
+    @BusObserver(["like5"])
+    fun test1(i: Int) {
+        Log.e(TAG, "BaseActivity1 test tag=like5，数据：$i")
+    }
 
 }
--- app/src/main/java/com/like/floweventbus/sample/MainViewModel.kt
+++ app/src/main/java/com/like/floweventbus/sample/MainViewModel.kt
@@ -6,9 +6,9 @@ import com.like.floweventbus_annotations.BusObserver
 
 class MainViewModel {
 
-//    @BusObserver(["like4"])
-//    fun test(i: Int) {
-//        Log.e(TAG, "MainViewModel test tag=like4，数据：$i")
-//    }
+    @BusObserver(["like4"])
+    fun test(i: Int) {
+        Log.e(TAG, "MainViewModel test tag=like4，数据：$i")
+    }
 
 }
