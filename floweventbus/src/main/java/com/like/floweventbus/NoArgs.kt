3c0b575101b9226f2e6c0ff2ded4341adda7fdc9
like <260864866@qq.com>
2021-07-24 12:34:51 +0800

    完善

--- floweventbus/src/main/java/com/like/floweventbus/NoArgs.kt
+++ floweventbus/src/main/java/com/like/floweventbus/NoArgs.kt
@@ -1,6 +1,7 @@
 package com.like.floweventbus
 
 /**
- * 用于 BusObserver 注解的方法没有参数时的处理
+ * 用于 BusObserver 注解的方法没有参数时的处理。
+ * 因为 flow.emit() 方法必须得传参数，如果直接传 null 的话，又会和那些可以为 null 的数据类型冲突。
  */
 class NoArgs
