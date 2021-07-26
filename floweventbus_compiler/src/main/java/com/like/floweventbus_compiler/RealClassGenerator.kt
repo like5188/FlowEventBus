b6152cf38bb9273ff08c842efa2ec2530a94066f
like5188 <260864866@qq.com>
2021-07-26 17:26:39 +0800

    完善

--- floweventbus_compiler/src/main/java/com/like/floweventbus_compiler/RealClassGenerator.kt
+++ floweventbus_compiler/src/main/java/com/like/floweventbus_compiler/RealClassGenerator.kt
@@ -3,10 +3,9 @@ package com.like.floweventbus_compiler
 import com.like.floweventbus_annotations.BusObserver
 import com.squareup.kotlinpoet.*
 import java.io.IOException
-import javax.lang.model.element.TypeElement
 
 /*
-object FlowEventbusCompiler {
+object FlowEventbusMethods {
     init {
         val methodInfoList = mutableListOf<MethodInfo>()
         for (methodInfo in methodInfoList) {
@@ -30,73 +29,70 @@ object FlowEventbusCompiler {
  * [BusObserver]注解的方法缓存类的代码。
  */
 object RealClassGenerator {
+    private const val PACKAGE_NAME = "com.like.floweventbus"
+    private const val CLASS_NAME = "FlowEventbusMethods"
+
     fun create(methodInfoList: List<MethodInfo>) {
         val filer = ProcessUtils.mFiler ?: return
-        methodInfoList.groupBy { it.hostClass }.forEach {
-            val hostClass = it.key
-            val hostMethodInfoList = it.value
-            val packageName = hostClass.asClassName().packageName
-            val className = "${hostClass.qualifiedName.toString().replace(".", "_")}_methods"
-            try {
-                // 创建包名及类的注释
-                FileSpec.builder(packageName, className)
-                    .addComment(" This codes are generated automatically by FlowEventBus. Do not modify!")// 类的注释
-                    .addType(createClass(hostClass, className, methodInfoList, hostMethodInfoList))
-                    .build()
-                    .writeTo(filer)
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
+        try {
+            // 创建包名及类的注释
+            FileSpec.builder(PACKAGE_NAME, CLASS_NAME)
+                .addComment(" This codes are generated automatically by FlowEventBus. Do not modify!")// 类的注释
+                .addType(createClass(methodInfoList))
+                .build()
+                .writeTo(filer)
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 
     /*
      * 创建类
      *
-     * object FlowEventbusCompiler {}
+     * object FlowEventbusMethods {}
      */
-    private fun createClass(
-        hostClass: TypeElement,
-        className: String,
-        methodInfoList: List<MethodInfo>,
-        hostMethodInfoList: List<MethodInfo>
-    ): TypeSpec {
-        val builder = TypeSpec.objectBuilder(className)
-            .addInitializerBlock(createInitializerBlock(hostClass, methodInfoList, hostMethodInfoList))
-        return builder.build()
+    private fun createClass(methodInfoList: List<MethodInfo>): TypeSpec {
+        return TypeSpec.objectBuilder(CLASS_NAME)
+            .addFunction(createInitFun(methodInfoList))
+            .build()
     }
 
-    private fun createInitializerBlock(
-        hostClass: TypeElement,
-        methodInfoList: List<MethodInfo>,
-        hostMethodInfoList: List<MethodInfo>
-    ): CodeBlock {
+    private fun createInitFun(methodInfoList: List<MethodInfo>): FunSpec {
+        return FunSpec.builder("init")
+            .addCode(createInitializerBlock(methodInfoList))
+            .build()
+    }
+
+    private fun createInitializerBlock(methodInfoList: List<MethodInfo>): CodeBlock {
         return buildCodeBlock {
-            hostMethodInfoList.forEach { hostMethodInfo ->
-                hostMethodInfo.tags.forEach { tag ->
-                    val requestCode = hostMethodInfo.requestCode
-                    val paramType = hostMethodInfo.paramType
-                    val isStickyMethod = methodInfoList.any {
-                        it.tags.contains(tag) && it.requestCode == requestCode && it.isSticky
+            methodInfoList.groupBy { it.hostClass }.forEach { entry ->
+                val hostClass = entry.key
+                val hostMethodInfoList = entry.value
+                hostMethodInfoList.forEach { hostMethodInfo ->
+                    hostMethodInfo.tags.forEach { tag ->
+                        val requestCode = hostMethodInfo.requestCode
+                        val paramType = hostMethodInfo.paramType
+                        val isStickyMethod = methodInfoList.any {
+                            it.tags.contains(tag) && it.requestCode == requestCode && it.isSticky
+                        }
+                        addStatement(
+                            "%T.addEvent(%S, %S, %S, %S, %L) { host, data ->",
+                            ClassName("com.like.floweventbus", "EventManager"),
+                            hostClass,
+                            tag,
+                            requestCode,
+                            hostMethodInfo.getJavaBoxParamType(),
+                            isStickyMethod
+                        )
+                        addStatement(
+                            "(host as %T).${hostMethodInfo.methodName}(${if (paramType == "com.like.floweventbus.NoArgs") "" else "data as ${hostMethodInfo.getKotlinParamType()}"});",
+                            hostClass.asClassName()
+                        )
+                        addStatement("}")
                     }
-                    addStatement(
-                        "%T.addEvent(%S, %S, %S, %S, %L) { host, data ->",
-                        ClassName("com.like.floweventbus", "EventManager"),
-                        hostClass,
-                        tag,
-                        requestCode,
-                        hostMethodInfo.getJavaBoxParamType(),
-                        isStickyMethod
-                    )
-                    addStatement(
-                        "(host as %T).${hostMethodInfo.methodName}(${if (paramType == "com.like.floweventbus.NoArgs") "" else "data as ${hostMethodInfo.getKotlinParamType()}"});",
-                        hostClass.asClassName()
-                    )
-                    addStatement("}")
                 }
             }
         }
     }
-
 
 }
