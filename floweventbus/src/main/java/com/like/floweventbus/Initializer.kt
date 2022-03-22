6d38c76348d562254599a28fc7079300a88a5fbb
like5188 <260864866@qq.com>
2022-03-22 10:45:44 +0800

    完善注释

--- floweventbus/src/main/java/com/like/floweventbus/Initializer.kt
+++ floweventbus/src/main/java/com/like/floweventbus/Initializer.kt
@@ -23,9 +23,12 @@ class Initializer {
             try {
                 val clazz = Class.forName(AUTO_GENERATED_CLASS)
                 clazz.getDeclaredMethod(INITIALIZE_METHOD).invoke(clazz.kotlin.objectInstance)
+                Log.e(TAG, "初始化成功")
             } catch (e: Exception) {
                 Log.e(TAG, "初始化失败 --> ${e.message}")
             }
+        } else {
+            Log.e(TAG, "已经初始化过了")
         }
     }
 }
