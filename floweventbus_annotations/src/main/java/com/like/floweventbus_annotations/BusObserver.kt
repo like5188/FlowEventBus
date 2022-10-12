6920f72d2d9b64e33c34840a233da3a4630c49da
like5188 <260864866@qq.com>
2022-10-12 16:53:42 +0800

    完善

--- floweventbus_annotations/src/main/java/com/like/floweventbus_annotations/BusObserver.kt
+++ floweventbus_annotations/src/main/java/com/like/floweventbus_annotations/BusObserver.kt
@@ -4,7 +4,7 @@ package com.like.floweventbus_annotations
 // SOURCE：注解仅存在于源码中，在class字节码文件中不包含
 // CLASS：默认的保留策略，注解会在class字节码文件中存在，但运行时无法获得，
 // RUNTIME：注解会在class字节码文件中存在，在运行时可以通过反射获取到
-@Retention(AnnotationRetention.RUNTIME)
+@Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.FUNCTION)
 @MustBeDocumented
 annotation class BusObserver(
