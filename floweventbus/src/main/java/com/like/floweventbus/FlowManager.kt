0245ac1c7cb8c4e9371b5f0af9dfe776842c619d
like <260864866@qq.com>
2023-04-12 10:16:37 +0800

    完善

--- floweventbus/src/main/java/com/like/floweventbus/FlowManager.kt
+++ floweventbus/src/main/java/com/like/floweventbus/FlowManager.kt
@@ -8,20 +8,6 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 object FlowManager {
     private val mFlowCache = mutableMapOf<String, MutableSharedFlow<Any?>>()
 
-    private fun findFlow(
-        tag: String,
-        requestCode: String,
-        isSticky: Boolean,
-        paramType: String,
-        isNullable: Boolean,
-    ): MutableSharedFlow<Any?>? {
-        val key = createKey(tag, requestCode, isSticky, paramType, isNullable)
-        if (mFlowCache.containsKey(key)) {
-            return mFlowCache[key]
-        }
-        return null
-    }
-
     fun findFlowOrCreateIfAbsent(
         tag: String,
         requestCode: String,
@@ -29,25 +15,17 @@ object FlowManager {
         paramType: String,
         isNullable: Boolean,
     ): MutableSharedFlow<Any?> {
-        var flow = findFlow(tag, requestCode, isSticky, paramType, isNullable)
+        val key = "$tag-$requestCode-$isSticky-$paramType-$isNullable"
+        var flow = mFlowCache[key]
         if (flow == null) {
             flow = MutableSharedFlow<Any?>(
                 replay = if (isSticky) 1 else 0,
                 extraBufferCapacity = if (isSticky) Int.MAX_VALUE else 0 // 避免挂起导致数据发送失败
             ).apply {
-                val key = createKey(tag, requestCode, isSticky, paramType, isNullable)
                 mFlowCache[key] = this
             }
         }
         return flow
     }
-
-    private fun createKey(
-        tag: String,
-        requestCode: String,
-        isSticky: Boolean,
-        paramType: String,
-        isNullable: Boolean,
-    ) = "$tag-$requestCode-$isSticky-$paramType-$isNullable"
 
 }
