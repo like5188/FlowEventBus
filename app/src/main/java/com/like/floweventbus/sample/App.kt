3699a48e6284a24be98262ab9e6ecb441a516bdb
like <260864866@qq.com>
2023-04-13 10:19:23 +0800

    ipc

--- app/src/main/java/com/like/floweventbus/sample/App.kt
+++ app/src/main/java/com/like/floweventbus/sample/App.kt
@@ -1,11 +1,19 @@
 package com.like.floweventbus.sample
 
 import android.app.Application
+import android.util.Log
 import com.like.floweventbus.FlowEventBus
+import com.like.floweventbus.TAG
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        Log.d(TAG, "App onCreate")
         FlowEventBus.init(this)
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        Log.d(TAG, "App onTerminate")
     }
 }
