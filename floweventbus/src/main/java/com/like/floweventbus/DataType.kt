a1b1f29b1fa06a27ac8c96c1765505908bc0cdaf
like <260864866@qq.com>
2023-04-14 12:32:15 +0800

    完善

--- floweventbus/src/main/java/com/like/floweventbus/DataType.kt
+++ floweventbus/src/main/java/com/like/floweventbus/DataType.kt
@@ -2,14 +2,6 @@ package com.like.floweventbus
 
 import kotlin.reflect.typeOf
 
-/**
- * 将 kotlin 数据类型转换成 java 数据类型。
- * 只是对 java 基本数据类型做了处理，把不为空的基本数据类型包装类转换成对应的基本数据类型。
- * 这样才能和[com.like.floweventbus.compiler.Generator]中存储的参数类型一样。
- * 比如：
- * java.lang.Byte 不为空 -> byte
- * java.lang.Byte 为空  -> java.lang.Byte
- */
 inline fun <reified T> toJavaDataType(): String {
     val isNullable = typeOf<T>().isMarkedNullable
     val canonicalName = T::class.java.canonicalName
@@ -17,8 +9,7 @@ inline fun <reified T> toJavaDataType(): String {
 }
 
 /**
- * 将 kotlin 数据类型[this]转换成 java 数据类型。
- * 只是对 java 基本数据类型做了处理，把不为空的基本数据类型包装类转换成对应的基本数据类型。
+ * 对 java 基本数据类型做了处理，把不为空的基本数据类型包装类转换成对应的基本数据类型。
  * 这样才能和[com.like.floweventbus.compiler.Generator]中存储的参数类型一样。
  * 比如：
  * java.lang.Byte 不为空 -> byte
